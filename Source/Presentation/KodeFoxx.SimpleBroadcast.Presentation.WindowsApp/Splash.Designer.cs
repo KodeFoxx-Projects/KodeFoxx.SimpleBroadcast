254287c0ee9361352fe8e3f208436492664a59c3
Yves Schelpe <yves.schelpe@kdg.be>
2022-10-29 22:28:42 +0200

    Splash screen added build version & preview 0.1 fixed for now.

--- Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Splash.Designer.cs
+++ Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Splash.Designer.cs
@@ -28,14 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.applicationNameAndVersionLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label1.Location = new System.Drawing.Point(25, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "S I M P L E";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label2.Location = new System.Drawing.Point(166, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "B R O A D C A S T";
+            // 
+            // applicationNameAndVersionLabel
+            // 
+            this.applicationNameAndVersionLabel.AutoSize = true;
+            this.applicationNameAndVersionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.applicationNameAndVersionLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.applicationNameAndVersionLabel.ForeColor = System.Drawing.Color.Azure;
+            this.applicationNameAndVersionLabel.Location = new System.Drawing.Point(30, 86);
+            this.applicationNameAndVersionLabel.Name = "applicationNameAndVersionLabel";
+            this.applicationNameAndVersionLabel.Size = new System.Drawing.Size(61, 15);
+            this.applicationNameAndVersionLabel.TabIndex = 4;
+            this.applicationNameAndVersionLabel.Text = "S I M P L E";
             // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 300);
+            this.ClientSize = new System.Drawing.Size(800, 400);
             this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.applicationNameAndVersionLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -45,10 +87,15 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Simple Broadcast";
             this.TopMost = true;
+            this.Load += new System.EventHandler(this.Splash_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private Label label1;
+        private Label label2;
+        private Label applicationNameAndVersionLabel;
     }
 }
--- Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Splash.cs
+++ Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Splash.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace KodeFoxx.SimpleBroadcast.Presentation.WindowsApp
 {
     public partial class Splash : Form
@@ -5,6 +7,33 @@ namespace KodeFoxx.SimpleBroadcast.Presentation.WindowsApp
         public Splash()
         {
             InitializeComponent();
+            ShowSplashImage();
+            ShowSplashText();
+        }
+
+        private void ShowSplashImage()
+        {
+            BackgroundImage = Image.FromFile(Path.Combine("Resources", "Images", "Splash.png"));
+            BackgroundImageLayout = ImageLayout.Stretch;
+            Width = 800;
+            Height = 400;
+            CenterToScreen();
+        }
+
+        private void ShowSplashText()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var dateTime = File.GetLastWriteTimeUtc(assembly.Location)
+                .ToString()
+                .Replace(" ", "-")
+                .Replace(":", "")
+                .Replace(@"/", "");
+            applicationNameAndVersionLabel.Text = $"preview version 0.1-{dateTime}";
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
