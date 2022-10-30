0ac2d3223012f06ce14a6ae69e8ea66787072f89
Yves Schelpe <yves.schelpe@kdg.be>
2022-10-30 17:04:01 +0100

    Mainpage buildup with tabs as menu, added header & footer for later usage when necessary.

--- Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Main.Designer.cs
+++ Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Main.Designer.cs
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.containerPanel = new System.Windows.Forms.Panel();
+            this.bodyPanel = new System.Windows.Forms.Panel();
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.footerPanel = new System.Windows.Forms.Panel();
+            this.containerPanel.SuspendLayout();
+            this.bodyPanel.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.Black;
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(1082, 60);
+            this.headerPanel.TabIndex = 0;
+            // 
+            // containerPanel
+            // 
+            this.containerPanel.Controls.Add(this.bodyPanel);
+            this.containerPanel.Controls.Add(this.footerPanel);
+            this.containerPanel.Controls.Add(this.headerPanel);
+            this.containerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.containerPanel.Location = new System.Drawing.Point(0, 0);
+            this.containerPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.containerPanel.Name = "containerPanel";
+            this.containerPanel.Size = new System.Drawing.Size(1082, 731);
+            this.containerPanel.TabIndex = 1;
+            // 
+            // bodyPanel
+            // 
+            this.bodyPanel.BackColor = System.Drawing.Color.White;
+            this.bodyPanel.Controls.Add(this.tabs);
+            this.bodyPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bodyPanel.Location = new System.Drawing.Point(0, 60);
+            this.bodyPanel.Margin = new System.Windows.Forms.Padding(9);
+            this.bodyPanel.Name = "bodyPanel";
+            this.bodyPanel.Padding = new System.Windows.Forms.Padding(16);
+            this.bodyPanel.Size = new System.Drawing.Size(1082, 643);
+            this.bodyPanel.TabIndex = 2;
+            // 
+            // tabs
+            // 
+            this.tabs.Controls.Add(this.tabPage1);
+            this.tabs.Controls.Add(this.tabPage2);
+            this.tabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabs.Location = new System.Drawing.Point(16, 16);
+            this.tabs.Margin = new System.Windows.Forms.Padding(16);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(1050, 611);
+            this.tabs.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1042, 578);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(874, 447);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // footerPanel
+            // 
+            this.footerPanel.BackColor = System.Drawing.Color.Sienna;
+            this.footerPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.footerPanel.Location = new System.Drawing.Point(0, 703);
+            this.footerPanel.Name = "footerPanel";
+            this.footerPanel.Size = new System.Drawing.Size(1082, 28);
+            this.footerPanel.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1082, 731);
+            this.Controls.Add(this.containerPanel);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Main";
             this.Text = "Main";
+            this.containerPanel.ResumeLayout(false);
+            this.bodyPanel.ResumeLayout(false);
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel headerPanel;
+        private Panel containerPanel;
+        private Panel footerPanel;
+        private Panel bodyPanel;
+        private TabControl tabs;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
--- Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Main.cs
+++ Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/Main.cs
@@ -4,5 +4,18 @@ namespace KodeFoxx.SimpleBroadcast.Presentation.WindowsApp;
 
 public partial class Main : BaseForm
 {
-    public Main(IMediator mediator) : base(mediator) { }
+    public Main(IMediator mediator) : base(mediator)
+    {
+        InitializeComponent();        
+        SetWindowTitle();
+        SetHeader();
+    }
+
+    private void SetHeader()
+    {
+        headerPanel.BackgroundImageLayout = ImageLayout.Stretch;
+        headerPanel.BackgroundImage = Image.FromFile(
+            Path.Combine("Resources", "Images", "Header.png")
+        );        
+    }
 }
