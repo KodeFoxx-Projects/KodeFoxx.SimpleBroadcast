ef12f3887914294582accb3ea5a51a6ec7ce3b7d
Yves Schelpe <yves.schelpe@kdg.be>
2022-11-01 14:42:45 +0100

    Icon on build

--- Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/BaseForm.Designer.cs
+++ Source/Presentation/KodeFoxx.SimpleBroadcast.Presentation.WindowsApp/BaseForm.Designer.cs
@@ -28,14 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseForm));
             this.SuspendLayout();
             // 
-            // Main
+            // BaseForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1129, 652);
-            this.Name = "Main";
+            this.ClientSize = new System.Drawing.Size(1290, 869);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "BaseForm";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";
